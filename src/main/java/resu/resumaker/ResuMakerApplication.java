42fc35506017c4e7d5a69d7a6e5f1aff1eb31469
Mohamad Saleh <mohsley@outlook.com>
2021-07-08 16:58:03 -0700

    Skeleton spring boot application by Tony & Mohamad

---
+++ src/main/java/resu/resumaker/ResuMakerApplication.java
@@ -0,0 +1,13 @@
+package resu.resumaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResuMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/resu/resumaker/ResuMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package resu.resumaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResuMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
