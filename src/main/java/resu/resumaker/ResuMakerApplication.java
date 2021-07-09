dd8b6bd53078e829c43e4fe6f4618dd71d8cc389
Tony Ma <51039795+tonyma08@users.noreply.github.com>
2021-07-09 16:19:39 -0700

    After using PdfBox, iText seems preferable

--- src/main/java/resu/resumaker/ResuMakerApplication.java
+++ src/main/java/resu/resumaker/ResuMakerApplication.java
@@ -4,6 +4,11 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,6 +26,12 @@ public class ResuMakerApplication {
 
     public static void main(String[] args) throws IOException, DocumentException, URISyntaxException {
         SpringApplication.run(ResuMakerApplication.class, args);
+
+        // iTextTest();
+        PdfBoxTest();
+    }
+
+    private static void iTextTest() throws FileNotFoundException, DocumentException {
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
 
@@ -54,6 +65,34 @@ public class ResuMakerApplication {
         table.addCell("r1, c1");
         table.addCell("row 1, col 2");
         table.addCell("row 1, col 3");
+    }
+
+    private static void PdfBoxTest() throws IOException, URISyntaxException {
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        contentStream.setFont(PDType1Font.COURIER, 12);
+        contentStream.beginText();
+        contentStream.showText("Hello World");
+        contentStream.endText();
+        contentStream.close();
+
+        newPdfBoxImage(document, page, "blah.jpeg");
+
+        document.save("pdfBoxHelloWorld.pdf");
+        document.close();
+    }
+
+    private static void newPdfBoxImage(PDDocument document, PDPage page, String imageLocation) throws URISyntaxException, IOException {
+        Path path = Paths.get(ClassLoader.getSystemResource(imageLocation).toURI());
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        PDImageXObject image
+                = PDImageXObject.createFromFile(path.toAbsolutePath().toString(), document);
+        contentStream.drawImage(image, 0, 0);
+        contentStream.close();
     }
 }
 
