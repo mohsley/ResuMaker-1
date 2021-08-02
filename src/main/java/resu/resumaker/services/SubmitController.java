108d9d03ee019fc77b6df5d70b82c01107b3067c
Mohamad Saleh <mohsley@outlook.com>
2021-08-02 15:16:17 -0700

    Working on front-end connection.

--- src/main/java/resu/resumaker/services/SubmitController.java
+++ src/main/java/resu/resumaker/services/SubmitController.java
@@ -1,20 +1,28 @@
 package resu.resumaker.services;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-//@Controller
+@Controller
 public class SubmitController {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     static final String USER = "program";
     static final String PASS = "1234";
 
-//    @PostMapping("/submit")
-//    public void submit(@RequestParam String[] contactPOST, @RequestParam String[] skillPOST, @RequestParam String[] workPOST, @RequestParam String[] educationPOST) {
-//        contactSubmit(contactPOST);
-//    }
+    @PostMapping("/submit")
+    public void submit(@RequestParam String[] contactPOST, @RequestParam String[] skillPOST,
+                       @RequestParam String[] workPOST, @RequestParam String[] educationPOST) {
+        contactSubmit(contactPOST);
+        skillSubmit(skillPOST);
+        workSubmit(workPOST);
+        educationSubmit(educationPOST);
+    }
 
     public static void contactSubmit(String[] contactPOST) {
         String values = "('"+ contactPOST[0] + "', '" + contactPOST[1] + "', '" + contactPOST[2] + "')";
